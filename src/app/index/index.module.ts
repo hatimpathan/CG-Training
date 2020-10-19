6b5133a569a2e58815d4c5e8bde4fec8a9252c26
Hatim <bob@example.com>
2020-10-19 11:59:24 +0530

    LoginCmp

--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { defineCustomElements } from 'index-design-system/dist/loader';
+import{IndexTextInput} from 'index-design-system/dist/loader';
 
 defineCustomElements();
+const Decla=[IndexTextInput];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [],
 })
 export class IndexModule { }
